0ca3148250cad77c83e8ebc19096eb7772bb62d9
Michael S <Michael.Stojanovic@hotmail.com>
2025-06-13 16:07:23 +0200

    added container component

---
+++ src/components/container/container.tsx
@@ -0,0 +1,13 @@
+import type { ReactNode } from 'react';
+
+const Container = ({ children }: { children: ReactNode }) => {
+  return (
+    <>
+      <div className='grid grid-cols-[20%_80%] p-4 h-screen gap-1 theme overflow-hidden '>
+        {children}
+      </div>
+    </>
+  );
+};
+
+export default Container;
