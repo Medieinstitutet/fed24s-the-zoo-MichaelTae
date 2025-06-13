0e7d2a45a3e17481addbdda0893b4dd57173683a
Michael S <Michael.Stojanovic@hotmail.com>
2025-06-13 16:06:57 +0200

    added context to app + localstorage theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import "./App.css";
-
+import { RouterProvider } from 'react-router';
+import './App.css';
+import { router } from './utils/router';
+import { AnimalContext } from './contexts/animalContext';
+import { useReducer } from 'react';
+import { AnimalReducer } from './reducers/animalReducer';
 function App() {
-  return <></>;
+  const [animal, dispatch] = useReducer(AnimalReducer, []);
+  window.onload = () => {
+    localStorage.getItem('theme')
+      ? document.documentElement.classList.add(localStorage.getItem('theme')!)
+      : document.documentElement.classList.add('light');
+    const html = document.querySelector('html');
+    if (html) {
+      html.classList.add('theme');
+    }
+  };
+  return (
+    <>
+      <AnimalContext.Provider value={{ animal, dispatch }}>
+        <RouterProvider router={router}></RouterProvider>
+      </AnimalContext.Provider>
+    </>
+  );
 }
 
 export default App;
