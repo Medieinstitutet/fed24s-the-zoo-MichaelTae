374ad995c29b9c00f50261888424f0372fe73820
Michael S <Michael.Stojanovic@hotmail.com>
2025-06-15 18:56:37 +0200

    added minor css change

--- src/components/container/container.tsx
+++ src/components/container/container.tsx
@@ -3,7 +3,7 @@ import type { ReactNode } from 'react';
 const Container = ({ children }: { children: ReactNode }) => {
   return (
     <>
-      <div className='grid grid-cols-[20%_80%] p-4 h-screen gap-1 theme overflow-hidden '>
+      <div className='grid grid-cols-[20%_80%] p-2 h-screen gap-1 theme overflow-hidden'>
         {children}
       </div>
     </>
