ce77c526b5f6c2d7e393233c7664956199e36e50
Michael S <Michael.Stojanovic@hotmail.com>
2025-06-15 18:59:43 +0200

    added minor css + motion func

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -2,6 +2,7 @@ import { Outlet } from 'react-router';
 import Container from '../components/container/container';
 import { Navbar } from '../components/nav/navbar';
 import { useFetchAnimals } from '../hooks/useFetchAnimals';
+import { AnimatePresence } from 'motion/react';
 
 export const Layout = () => {
   useFetchAnimals();
@@ -10,8 +11,10 @@ export const Layout = () => {
     <Container>
       <Navbar />
 
-      <main className='justify-center border flex h-screen '>
-        <Outlet />
+      <main className='justify-center border-l theme border-t  flex h-screen layout'>
+        <AnimatePresence mode='wait'>
+          <Outlet key={location.pathname} />
+        </AnimatePresence>
       </main>
       {/* <footer>
         <p>Footer content goes here.</p>
