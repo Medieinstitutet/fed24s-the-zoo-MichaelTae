1404ef55f73e780e08070a3ccb482bd9e64f1949
Michael S <Michael.Stojanovic@hotmail.com>
2025-06-17 12:56:52 +0200

    added mediaquery check in container

--- src/components/container/container.tsx
+++ src/components/container/container.tsx
@@ -1,9 +1,15 @@
 import type { ReactNode } from 'react';
+import { useMediaQueryContext } from '../../contexts/MediaQueryContext';
 
 const Container = ({ children }: { children: ReactNode }) => {
+  const { isMobile, isTablet, isDesktop } = useMediaQueryContext();
   return (
     <>
-      <div className='grid grid-cols-[20%_80%] p-2 h-screen gap-1 theme overflow-hidden'>
+      <div
+        className={`grid ${
+          isDesktop ? 'grid-cols-[20%_80%]' : 'grid-cols-1'
+        } p-2 h-screen gap-1 theme overflow-hidden`}
+      >
         {children}
       </div>
     </>
