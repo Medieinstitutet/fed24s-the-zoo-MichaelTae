73ce690aa64de8a60e60f6e1999e34dac9e75a39
Michael S <Michael.Stojanovic@hotmail.com>
2025-06-17 12:59:54 +0200

    added mediaquery context to layout

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -3,22 +3,23 @@ import Container from '../components/container/container';
 import { Navbar } from '../components/nav/navbar';
 import { useFetchAnimals } from '../hooks/useFetchAnimals';
 import { AnimatePresence } from 'motion/react';
+import { useMediaQueryContext } from '../contexts/MediaQueryContext';
+import { MobileNav } from '../components/nav/mobileNav';
 
 export const Layout = () => {
   useFetchAnimals();
+  const { isMobile, isTablet, loading } = useMediaQueryContext();
+
+  if (loading) return null;
 
   return (
     <Container>
-      <Navbar />
-
-      <main className='justify-center border-l theme border-t  flex h-screen layout'>
+      {!isMobile && !isTablet ? <Navbar /> : <MobileNav />}
+      <main className='justify-center border-l theme border-t  flex h-screen layout '>
         <AnimatePresence mode='wait'>
           <Outlet key={location.pathname} />
         </AnimatePresence>
       </main>
-      {/* <footer>
-        <p>Footer content goes here.</p>
-      </footer> */}
     </Container>
   );
 };
